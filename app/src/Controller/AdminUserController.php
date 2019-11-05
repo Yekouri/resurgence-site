63bb03e0ce2d1fa0939e357b01725531bf4069ad
Yekouri <Trashskillz@gmail.com>
2019-11-05 15:57:38 +0100

    Added user creation and update with security and roles. And fixed apache server to allow multiple routes with Symfony

---
+++ app/src/Controller/AdminUserController.php
@@ -0,0 +1,72 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\Security\Core\Encoder\UserPasswordEncoderInterface;
+use Symfony\Component\HttpFoundation\Request;
+use Doctrine\DBAL\Exception\UniqueConstraintViolationException;
+use App\Entity\User;
+
+/**
+ * @Route("/admin/user")
+ */
+class AdminUserController extends AbstractController
+{
+    /**
+     * @Route("/", name="user_list")
+     */
+    public function userList()
+    {
+        $users = $this->getDoctrine()->getRepository
+        (User::class)->findAll();
+
+        return $this->render('Admin/user/user_list.html.twig', [
+            'users' => $users
+        ]);
+    }
+
+    /**
+     * @Route("/create", name="user_create", methods={"GET","POST"})
+     */
+    public function userCreate(Request $request, UserPasswordEncoderInterface $encoder) 
+    {
+        $message = '';
+        $error = false;
+        if ($request->isMethod('POST')) 
+        {
+            $postdata = $request->request->all();
+
+            $entityManager = $this->getDoctrine()->getManager();
+
+            $user = new User();
+            $user->setUsername($postdata['username']);
+            $user->setPassword(
+                $encoder->encodePassword($user, $postdata['password'])
+            );
+
+            $roles = 'ROLE_ADMIN' == $postdata['roles'] ? ['ROLE_ADMIN'] : [];
+
+            $user->setRoles($roles);
+    
+            try {
+                $entityManager->persist($user);
+                $entityManager->flush();
+                $message = 'Created user with username: "'. $postdata['username'] . '"';
+            } catch (UniqueConstraintViolationException $e) {
+                $message = 'Failed: duplicate entry for user "'. $postdata['username'] . '"';
+                $error = true;
+            } catch (\Exception $e) {
+                $message = 'Failed: creating user: "'. $postdata['username'] . '"';
+                $error = true;
+            }
+
+        }
+
+        return $this->render('Admin/user/user_create.html.twig', [
+            'success_message' => $message,
+            'error' => $error
+        ]);
+    }
+}
