383088ea01b5e6fc65c731a958a8330a5f1a2555
Yekouri <Trashskillz@gmail.com>
2019-11-05 17:51:19 +0100

    Added js to templates and support for ES6

--- app/gulpfile.js
+++ app/gulpfile.js
@@ -3,10 +3,11 @@ const plumber = require('gulp-plumber');
 const autoprefixer = require('autoprefixer');
 const concat = require('gulp-concat');
 const postcss = require('gulp-postcss');
-const rename = require('gulp-rename');
+const uglify = require('gulp-uglify');
 const sourcemaps = require('gulp-sourcemaps');
 const clean = require('postcss-discard-comments');
 const cleanCSS = require('gulp-clean-css');
+const babel = require('gulp-babel');
 const url = require('postcss-url');
 
 gulp.task('build:css', function() {
@@ -27,6 +28,19 @@ gulp.task('build:css', function() {
         .pipe(gulp.dest('./public/css/'));
 });
 
+gulp.task('build:js', function() {
+    return gulp.src([
+        'assets/js/*.js'
+        ])
+        .pipe(babel({presets: ['@babel/env']}))
+        .pipe(concat('main.js'))
+        .pipe(uglify())
+        .pipe(gulp.dest('./public/js/'));
+});
+
+gulp.task('build', gulp.series(['build:css', 'build:js']));
+
 gulp.task('watch:css', function() {
     gulp.watch('assets/css/*.cs', ['build:css']);
+    gulp.watch('assets/js/*.js', ['build:js']);
 })
---
+++ app/public/js/main.js
@@ -0,0 +1,1 @@
+"use strict";var k=5;
