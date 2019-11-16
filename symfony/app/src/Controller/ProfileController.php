fac8df598d63cebab9a22bff94bb66c4cdfde03b
Yekouri <Trashskillz@gmail.com>
2019-11-16 19:00:40 +0100

    Fixed profile issues

---
+++ symfony/app/src/Controller/ProfileController.php
@@ -0,0 +1,210 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\HttpFoundation\Request;
+use App\Entity\Attunement;
+use App\Entity\Rank;
+use App\Entity\Profile;
+
+
+/**
+ * @Route("/profile")
+ */
+class ProfileController extends AbstractController
+{
+    /**
+     * @Route("/create", name="profile_create")
+     */
+    public function createProfile(Request $request)
+    {
+        $attunements = $this->getDoctrine()->getRepository
+        (Attunement::class)->findAll();
+
+        $ranks = $this->getDoctrine()->getRepository
+        (Rank::class)->findAll();
+
+        if ($request->isMethod('POST'))
+        {
+            $postdata = $request->request->all();
+
+            $entityManager = $this->getDoctrine()->getManager();
+
+            $profile = new Profile();
+            $profile->setUser($this->getUser());
+            $profile->setName($postdata['name']);
+            $profile->setRace($postdata['race']);
+            $profile->setClass($postdata['class']);
+            $profile->setSpec($postdata['spec']);
+            $profile->setJoinDate(new \DateTime($postdata['join_date']));
+            $profile->setPvp(!empty($postdata['pvp']) ? true : false);
+            $profile->setCurrentPvpRank(!empty($postdata['current_pvp_rank']) ? $postdata['current_pvp_rank'] : 0);
+            $profile->setGoalPvpRank(!empty($postdata['goal_pvp_rank']) ? $postdata['goal_pvp_rank'] : 0);
+
+            if (!empty($postdata['profession_A'])) {
+                $profile->setProfessionA($postdata['profession_A']);
+            }
+
+            if (!empty($postdata['profession_B'])) {
+                $profile->setProfessionB($postdata['profession_B']);
+            }
+
+            if (!empty($postdata['attunement'])) {
+                foreach ($postdata['attunement'] as $key => $value) {
+                    $profile->addAttunement($attunements[$key]);
+                }
+            }
+
+            foreach ($ranks as $value) {
+                if ($postdata['rank'] == $value->getName()){
+                    $profile->setRank($value);
+                }
+            }
+
+            try {
+                $entityManager->persist($profile);
+                $entityManager->flush();
+            } catch (\Exception $e) {
+                print_r($e->getMessage());
+                die;
+            }
+
+        }
+
+        return $this->render('Profile/profile-create-form.html.twig', [
+            'attunements' => $attunements,
+            'ranks' => $ranks,
+            'races' => $this->getRaces(),
+            'classes' => $this->getClasses(),
+            'professions' => $this->getProfessions(),
+            'specs' => $this->getSpecs()
+        ]);
+    }
+
+    /**
+     * @Route("/", name="profile_my")
+     */
+    public function myProfile() {
+        $repository = $this->getDoctrine()->getRepository(Profile::class);
+        $results = $repository->findProfilesByUser($this->getUser()->getId());
+      
+        return $this->render('Profile/profile-my.html.twig', [
+            'profiles' => $results
+        ]);
+    }
+
+    /**
+     * @Route("/edit", name="profile_edit")
+     */
+    public function editProfile(Request $request) {
+        $id = $request->query->get('id');
+
+        $attunements = $this->getDoctrine()->getRepository
+        (Attunement::class)->findAll();
+
+        $ranks = $this->getDoctrine()->getRepository
+        (Rank::class)->findAll();
+
+        // Only find if profile id is linked to the user id
+        $repository = $this->getDoctrine()->getRepository(Profile::class);
+        $profile = $repository->findProfile($id, $this->getUser()->getId());
+
+        if ($request->isMethod('POST'))
+        {
+            $postdata = $request->request->all();
+
+            $entityManager = $this->getDoctrine()->getManager();
+
+            $profile->setSpec($postdata['spec']);
+            $profile->setJoinDate(new \DateTime($postdata['join_date']));
+            $profile->setPvp(!empty($postdata['pvp']) ? true : false);
+            $profile->setCurrentPvpRank(!empty($postdata['current_pvp_rank']) ? $postdata['current_pvp_rank'] : 0);
+            $profile->setGoalPvpRank(!empty($postdata['goal_pvp_rank']) ? $postdata['goal_pvp_rank'] : 0);
+
+            if (!empty($postdata['profession_A'])) {
+                $profile->setProfessionA($postdata['profession_A']);
+            }
+
+            if (!empty($postdata['profession_B'])) {
+                $profile->setProfessionB($postdata['profession_B']);
+            }
+
+            if (!empty($postdata['attunement'])) {
+                foreach ($postdata['attunement'] as $key => $value) {
+                    $profile->addAttunement($attunements[$key]);
+                }
+            }
+
+            foreach ($ranks as $value) {
+                if ($postdata['rank'] == $value->getName()){
+                    $profile->setRank($value);
+                }
+            }
+
+            try {
+                $entityManager->persist($profile);
+                $entityManager->flush();
+            } catch (\Exception $e) {
+                print_r($e->getMessage());
+                die;
+            }
+        }
+
+        return $this->render('Profile/profile-edit-form.html.twig', [
+            'profile' => $profile,
+            'attunements' => $attunements,
+            'ranks' => $ranks,
+            'professions' => $this->getProfessions(),
+            'specs' => $this->getSpecs()
+        ]);
+    }
+
+    private function getRaces() {
+        return [
+            'Human',
+            'Dwarf',
+            'Night Elf',
+            'Gnome'
+        ];
+    }
+
+    private function getClasses() {
+        return [
+            'Druid',
+            'Hunter',
+            'Mage',
+            'Paladin',
+            'Priest',
+            'Rogue',
+            'Warlock',
+            'Warrior',
+        ];
+    }
+
+    private function getSpecs() {
+        return [
+            'Dps',
+            'Tank',
+            'Healer',
+        ];
+    }
+
+    private function getProfessions() {
+        return [
+            '',
+            'Alchemy',
+            'Blacksmithing (Armour)',
+            'Blacksmithing (Weapon)',
+            'Enchanting',
+            'Engineering (Gnomish)',
+            'Engineering (Goblin)',
+            'Herbalism',
+            'Leatherworking',
+            'Mining',
+            'Skinning',
+            'Tailoring',
+        ];
+    }
+}
---
+++ symfony/app/src/Migrations/Version20191116172253.php
@@ -0,0 +1,48 @@
+<?php
+
+declare(strict_types=1);
+
+namespace DoctrineMigrations;
+
+use Doctrine\DBAL\Schema\Schema;
+use Doctrine\Migrations\AbstractMigration;
+
+/**
+ * Auto-generated Migration: Please modify to your needs!
+ */
+final class Version20191116172253 extends AbstractMigration
+{
+    public function getDescription() : string
+    {
+        return '';
+    }
+
+    public function up(Schema $schema) : void
+    {
+        // this up() migration is auto-generated, please modify it to your needs
+        $this->abortIf($this->connection->getDatabasePlatform()->getName() !== 'mysql', 'Migration can only be executed safely on \'mysql\'.');
+
+        $this->addSql('CREATE TABLE attunement (id INT AUTO_INCREMENT NOT NULL, name VARCHAR(255) NOT NULL, PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci ENGINE = InnoDB');
+        $this->addSql('CREATE TABLE profile (id INT AUTO_INCREMENT NOT NULL, rank_id INT NOT NULL, user_id INT NOT NULL, name VARCHAR(255) NOT NULL, race VARCHAR(255) NOT NULL, class VARCHAR(255) NOT NULL, spec VARCHAR(255) NOT NULL, pvp TINYINT(1) NOT NULL, current_pvp_rank INT NOT NULL, goal_pvp_rank INT NOT NULL, profession_a VARCHAR(255) DEFAULT NULL, profession_b VARCHAR(255) DEFAULT NULL, join_date DATE NOT NULL, INDEX IDX_8157AA0F7616678F (rank_id), INDEX IDX_8157AA0FA76ED395 (user_id), PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci ENGINE = InnoDB');
+        $this->addSql('CREATE TABLE profile_attunement (profile_id INT NOT NULL, attunement_id INT NOT NULL, INDEX IDX_69AB8154CCFA12B8 (profile_id), INDEX IDX_69AB8154406B359F (attunement_id), PRIMARY KEY(profile_id, attunement_id)) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci ENGINE = InnoDB');
+        $this->addSql('CREATE TABLE rank (id INT AUTO_INCREMENT NOT NULL, name VARCHAR(255) NOT NULL, PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci ENGINE = InnoDB');
+        $this->addSql('ALTER TABLE profile ADD CONSTRAINT FK_8157AA0F7616678F FOREIGN KEY (rank_id) REFERENCES rank (id)');
+        $this->addSql('ALTER TABLE profile ADD CONSTRAINT FK_8157AA0FA76ED395 FOREIGN KEY (user_id) REFERENCES user (id)');
+        $this->addSql('ALTER TABLE profile_attunement ADD CONSTRAINT FK_69AB8154CCFA12B8 FOREIGN KEY (profile_id) REFERENCES profile (id) ON DELETE CASCADE');
+        $this->addSql('ALTER TABLE profile_attunement ADD CONSTRAINT FK_69AB8154406B359F FOREIGN KEY (attunement_id) REFERENCES attunement (id) ON DELETE CASCADE');
+    }
+
+    public function down(Schema $schema) : void
+    {
+        // this down() migration is auto-generated, please modify it to your needs
+        $this->abortIf($this->connection->getDatabasePlatform()->getName() !== 'mysql', 'Migration can only be executed safely on \'mysql\'.');
+
+        $this->addSql('ALTER TABLE profile_attunement DROP FOREIGN KEY FK_69AB8154406B359F');
+        $this->addSql('ALTER TABLE profile_attunement DROP FOREIGN KEY FK_69AB8154CCFA12B8');
+        $this->addSql('ALTER TABLE profile DROP FOREIGN KEY FK_8157AA0F7616678F');
+        $this->addSql('DROP TABLE attunement');
+        $this->addSql('DROP TABLE profile');
+        $this->addSql('DROP TABLE profile_attunement');
+        $this->addSql('DROP TABLE rank');
+    }
+}
